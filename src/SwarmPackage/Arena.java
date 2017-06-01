e58a152f782ae344095751bd85f21669d5c801e5
jproney <pdotjpr@gmail.com>
2017-06-01 12:09:34 -0700

    Breeding slider

--- src/SwarmPackage/Arena.java
+++ src/SwarmPackage/Arena.java
@@ -6,15 +6,31 @@ import javax.swing.JPanel;
 import java.util.ArrayList;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.JSlider;
 
-public class Arena extends JPanel implements Runnable{
+public class Arena extends JPanel implements Runnable, ChangeListener{
 	
-	
+	private double breedLevel = 0.0;
+	private JSlider breedSlider;
 	private static long GAME_TICK = 20 * 1000; //20000 nanoseconds = 20 ms
 	private ArrayList<Sprite> orgList;
 	
 	public Arena(){
 		orgList = new ArrayList<Sprite>();
+		breedSlider = new JSlider(JSlider.HORIZONTAL,0, 100, 0);
+		breedSlider.setMajorTickSpacing(10);
+		breedSlider.addChangeListener(this);
+		this.add(breedSlider);
+     	
+	}
+	
+	public void stateChanged(ChangeEvent e){
+		JSlider source = (JSlider)e.getSource();
+    	if (!source.getValueIsAdjusting()) {
+        	breedLevel = source.getValue();
+        } 
 	}
 	
 	public void addOrganism(Sprite o){
@@ -22,7 +38,7 @@ public class Arena extends JPanel implements Runnable{
 	}
 	
 	public void breed(){
-		if(Math.random() >  (1 - .00007*orgList.size())){
+		if(Math.random() >  (1 - .0000015*breedLevel*orgList.size())){
 			addOrganism(new Prey((int)(Math.random()*800),(int)(Math.random()*800)));
 		}
 	}
